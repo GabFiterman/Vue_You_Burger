e872390b6cebaabec5494c8cc5121980d6906f31
Fiterman <gfiterman96@gmail.com>
2022-06-21 16:10:27 -0300

    Data() no PrimeiroComponent.vue
    Adicionada a propriedade data() no 'PrimeiroComponente' e reflexão dos dados no template.

--- vue_You_Burger/src/components/PrimeiroComponente.vue
+++ vue_You_Burger/src/components/PrimeiroComponente.vue
@@ -1,9 +1,22 @@
 <template>
-    <h1>Olá Vue!</h1>
+    <h1>Bem vindo, {{ nome }}</h1>
+    <ul>
+        <li>Nome: {{ nome }} {{ sobrenome }}</li>
+        <li>Idade: {{ idade }}</li>
+        <li>Profissao: {{ profissao }}</li>
+    </ul>
 </template>
 
 <script>
 export default {
-    name: 'PrimeiroComponente'
+    name: 'PrimeiroComponente',
+    data() {
+        return {
+            nome: 'Gabriel',
+            sobrenome: 'Fiterman',
+            idade: 23,
+            profissao: 'Desenvolvedor'
+        }
+    }
 }
 </script>
