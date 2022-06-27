6f572f56952f4beb97c577135c7894db809a335c
Fiterman <gfiterman96@gmail.com>
2022-06-27 15:37:42 -0300

    Início projeto - MyBurger
    Deletado todo o conteúdo de estudo inicial - Mantido na branch 'Vue_Conceitos'

--- vue_You_Burger/src/App.vue
+++ vue_You_Burger/src/App.vue
@@ -1,35 +1,10 @@
 <template>
-  <Header :esta_logado=true />
-  <PrimeiroComponente />
-  <LifeCycle />
-  <Pessoa />
+  <h1>Hello World</h1>
 </template>
 
 <script>
-import PrimeiroComponente from './components/PrimeiroComponente.vue';
-import LifeCycle from './components/LifeCycle.vue';
-import Pessoa from './components/Pessoa.vue';
-import Header from './components/Header.vue';
 
-export default {
-  name: 'App',
-  components: {
-    PrimeiroComponente,
-    LifeCycle,
-    Pessoa,
-    Header
-  }
-}
 </script>
 
 <style>
-body {
-  background-color: rgb(50, 50, 50);
-  color: white;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-}
-
-a {
-  color: rgb(147, 217, 218)
-}
 </style>
